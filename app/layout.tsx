5fe682a0965dd052eff603ab0ba3bc4c37dca74d
GonzaloSevilla22 <gonzalosevilla11@gmail.com>
2026-04-18 00:01:56 -0300

    fix vista movil

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   themeColor: '#09090b',
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 5,
 }
 
 export default function RootLayout({
